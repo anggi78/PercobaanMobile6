9dd8031b499000eba44a75e9f8f3288c6381e998
sriputrianti <putrimateka@gmail.com>
2022-11-20 19:26:11 +0800

    Initial commit

---
+++ app/src/main/java/com/aisyahrahmadanipohontu_f55121054/percobaanmobile6/ContentActivity.java
@@ -0,0 +1,60 @@
+package com.aisyahrahmadanipohontu_f55121054.percobaanmobile6;
+
+import static com.google.android.material.bottomnavigation.BottomNavigationView.*;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.aisyahrahmadanipohontu_f55121054.percobaanmobile6.databinding.ActivityContentBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.aisyahrahmadanipohontu_f55121054.percobaanmobile6.databinding.ActivityContentBinding;
+
+public class ContentActivity extends AppCompatActivity {
+    private ActivityContentBinding binding;
+    private BottomNavigationView.OnNavigationItemSelectedListener
+            mOnNavigationItemSelectionListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    switchFragment (fragment);
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragment = new DashboardFragment();
+                    switchFragment (fragment);
+                    return true;
+                case R.id.navigation_account:
+                    fragment = new AccountFragment();
+                    switchFragment (fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content);
+        binding = ActivityContentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectionListener);
+
+        if (savedInstanceState == null) {
+            binding.navigation.setSelectedItemId(R.id.navigation_home);
+        }
+    }
+    private void switchFragment (Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, fragment,
+                fragment.getClass().getSimpleName()).commit();
+    }
+}
